7e10ee1dd5e01eadc0f4f8c880791366766fac4f
LuYidan <3332981531@qq.com>
2022-09-02 15:58:23 +0800

    对于检测是否是gpu版本的opencv灵活一些 接收到q时，程序终止 接收到p时，暂停3s

--- aimbot.py
+++ aimbot.py
@@ -1,19 +1,50 @@
+import keyboard
 import numpy as np
 import pyautogui
-import win32api, win32con, win32gui
+import win32api, win32con, win32gui,win32ui
 import cv2
 import math
 import time
 
+'''
+Fastest way to take a screenshot with python on windows
+https://blog.csdn.net/jokerzhanglin/article/details/117201541
+https://stackoverflow.com/questions/3586046/fastest-way-to-take-a-screenshot-with-python-on-windows
+'''
+def window_capture(filename):
+    hwnd = 0  # 窗口的编号，0号表示当前活跃窗口
+    # 根据窗口句柄获取窗口的设备上下文DC（Divice Context）
+    hwndDC = win32gui.GetWindowDC(hwnd)
+    # 根据窗口的DC获取mfcDC
+    mfcDC = win32ui.CreateDCFromHandle(hwndDC)
+    # mfcDC创建可兼容的DC
+    saveDC = mfcDC.CreateCompatibleDC()
+    # 创建bigmap准备保存图片
+    saveBitMap = win32ui.CreateBitmap()
+    # 获取监控器信息
+    MoniterDev = win32api.EnumDisplayMonitors(None, None)
+    w = MoniterDev[0][2][2]
+    h = MoniterDev[0][2][3]
+    # print w,h　　　#图片大小
+    # 为bitmap开辟空间
+    saveBitMap.CreateCompatibleBitmap(mfcDC, w, h)
+    # 高度saveDC，将截图保存到saveBitmap中
+    saveDC.SelectObject(saveBitMap)
+    # 截取从左上角（0，0）长宽为（w，h）的图片
+    saveDC.BitBlt((0, 0), (w, h), mfcDC, (0, 0), win32con.SRCCOPY)
+    saveBitMap.SaveBitmapFile(saveDC, filename)
+
 CONFIG_FILE = './yolov3_416.cfg'
 WEIGHT_FILE = './yolov3_416.weights'
-# CONFIG_FILE = 'yolov3-tiny.cfg'
-# WEIGHT_FILE = './yolov3-tiny_last.weights'
-
 net = cv2.dnn.readNetFromDarknet(CONFIG_FILE, WEIGHT_FILE)
-net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
-# net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
-# net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+#如果你使用的cv2支持GPU
+if cv2.cuda.getCudaEnabledDeviceCount():    
+    net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+    net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+#如果你使用的opencv只支持CPU
+else:   
+    net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+
 
 # 获取网络所有层的名称
 ln = net.getLayerNames()
@@ -26,7 +57,7 @@ size_scale = 2
 while True:
     start_time = time.time()
 
-    # 获得窗口矩形框的大小（left,top,right,bottom）
+    # 获得窗口矩形框的大小（left,top,right,bottom）->（left,top,w,h）
     rect = win32gui.GetWindowRect(hwnd)
     region = rect[0], rect[1], rect[2] - rect[0], rect[3] - rect[1]
 
@@ -92,7 +123,7 @@ while True:
                 min_at = i
 
         # boxes[min_at] = [x,y,w,h]; 这里x,y是框左上角的坐标
-        # 经过如下重新计算，获得人物头的坐标。x = x + w/2 + left_top; y = y + h/4 + right_top
+        # 经过如下重新计算，获得人物头的坐标。x = x + w/2 + left_top; y = y + h/7 + right_top
         # 注意，此时获得的坐标是相对于游戏窗口的左上角而言的，
         x = int(boxes[min_at][0] + boxes[min_at][2]/2) + rect[0]
         y = int(boxes[min_at][1] + boxes[min_at][3]/7) + rect[1]# For head shot
@@ -112,3 +143,13 @@ while True:
     frame = cv2.resize(frame, (int(frame.shape[1] / size_scale), int(frame.shape[0] / size_scale)))
     cv2.imshow("frame", frame)
     cv2.waitKey(1)
+
+    # 接收到q时，程序终止
+    if keyboard.is_pressed('q'):  # if key 'q' is pressed
+        print('QUIT!')
+        cv2.destroyAllWindows()
+        break  # finishing the loop
+    # 接收到p时，暂停3s
+    if keyboard.is_pressed('p'):  # if key 'q' is pressed
+        print('PARSE 3S')
+        time.sleep(3)
